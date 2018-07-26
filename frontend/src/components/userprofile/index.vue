83950058329d99468bba59c929ac60f3fd9e6510
copervan <copervan@163.com>
2018-07-26 08:48:35 +0800

    2018-07-6-08-48

--- frontend/src/components/userprofile/index.vue
+++ frontend/src/components/userprofile/index.vue
@@ -21,7 +21,15 @@
 					{
 						title: "device_no",
 						key : "device_no"
-					}]
+					}],
+				formLeft: {
+                    input1: '',
+                    input2: '',
+                    input3: ''
+                },
+                modal6: false,
+                loading: true,
+                currentuser : {}
 			}
 		},
 		methods: {
@@ -44,10 +52,59 @@
 					}
 				}).done(callback)
 			},
+			creat_device(user,callback){
+				var mytoken = this.$store.state.token
+				var myusers = $.ajax({
+					url:"/rocky/device/",
+					type:"POST",
+					contentType: 'application/json'	,
+					beforeSend: function(xhr){
+						xhr.setRequestHeader("Authorization", mytoken);
+					},
+					data: JSON.stringify(user),
+					success: function(data){
+						console.log(data)
+						// callback =  data
+					},
+					error: function(err){
+						console.log(err)
+						alert(err)
+					}
+				}).done(callback)
+			},
 			setusers(data) {
 				console.log(data)
 				this.users1 = data
-			}
+			},
+			asyncOK () {
+				var _this = this
+				var currentuser = this.currentuser
+				currentuser.project_id = 1
+				currentuser.channel_id = 1
+				console.log(JSON.stringify(currentuser) )
+				_this.creat_device(currentuser,function(data){
+					alert(data)
+					_this.get_device(_this.setusers)
+					setTimeout(() => {
+                    	_this.modal6 = false;
+                	}, 2000);
+				}).bind                
+            },
+            select_current_user(currentRow,oldCurrentRow){
+            	console.log(currentRow,oldCurrentRow)
+            	this.currentuser = currentRow
+            },
+            row_dblclick(currentRow,index){
+            	this.currentuser = currentRow
+            	this.modal6 = true
+            },
+            creat_user() {
+            	this.currentuser = {id:'', device_no: '', project_id: 1, user_name:'' , channel_id: 1}
+            	this.modal6 = true
+            },
+            delete_user() {
+            	
+            }
 		},
 		created: function(){
 			this.get_device(this.setusers)
@@ -74,8 +131,36 @@
 		<button v-on:click="get_device()">Submmit!</button>
 		<div v-if="users1">
 			{{users1}}
-		<Table :columns="columns1" :data="data1"></Table>
+			<br/>
+		<Button type="primary" @click="modal6 = true">编辑用户</Button>
+		<Button type="primary" @click="creat_user">创建用户</Button>
+		<Button type="primary" @click="delete_user">创建用户</Button>
+		<Table highlight-row ref="currentRowTable" @on-current-change="select_current_user" 
+			@on-row-dblclick="row_dblclick" :columns="columns1" :data="data1"></Table>
 		<!-- <showuser  ></showuser> -->
+		
+	    <Modal
+	        v-model="modal6"
+	        title="Title"
+	        :loading="loading"
+	        @on-ok="asyncOK">
+	        <p>After you click ok, the dialog box will close in 2 seconds.</p>
+	    <template>
+			<Form :model="formLeft" label-position="left" :label-width="100">
+		        <FormItem label="ID">
+		            <Input v-model="currentuser.id"></Input>
+		        </FormItem>
+		        <FormItem label="device_no">
+
+		            <Input v-model="currentuser.device_no"></Input>
+		        </FormItem>
+		        <FormItem label="user_name">
+		            <Input v-model="currentuser.user_name"></Input>
+		        </FormItem>
+		    </Form>
+		</template>
+	    </Modal>
+		
 		</div>
 	</div>
 </template>
