42b4e001de5a09062f6f527072ea164ce6f070b7
copervan <copervan@163.com>
2018-07-30 18:52:33 +0800

    2018-07-30 18:52

--- frontend/src/components/diary/index.vue
+++ frontend/src/components/diary/index.vue
@@ -1,34 +1,3 @@
-<template>
-	<div>
-		<Button type="primary" @click="diary_add = true">create a diary</Button>
-		<div v-for="diary in diary_list">
-		    <div style="background:#eee;padding: 20px" v-on:click="show_detail(diary)" >
-		        <Card :bordered="false" >
-		            <p slot="title">{{diary.title}}</p>
-		            <p>{{diary.content}}</p>
-		            <p>{{diary.date}}</p>
-		        </Card>
-		    </div>
-    	</div> 
-	    <Modal
-	        v-model="diary_add"
-	        title="Create the diary"
-	        :loading="loading"
-	        @on-ok="add_diary">
-	        <p>After you click ok, the dialog box will close in 2 seconds.</p>
-		    <template>
-				<Form :model="formLeft" label-position="top" :label-width="300">
-			        <FormItem label="title">
-			            <Input v-model="currentdiary.title"></Input>
-			        </FormItem>
-			        <FormItem label="content">
-			            <Input v-model="currentdiary.content" type="textarea" :autosize="{minRows: 5,maxRows: 15}" placeholder="Enter something..."></Input>
-			        </FormItem>
-			    </Form>
-			</template>
-	    </Modal>
-	</div>
-</template>
 
 <script type="text/javascript">
 	import $ from 'jquery'
@@ -38,12 +7,28 @@
 			return {
 				diary_list: [],
 				currentdiary : {title:'',content:""},
-				diary_add : false
+				diary_add : false,
+				columns1: [
+					{
+						title: "id",
+						key : "id"
+					},
+					{
+						title: "title",
+						key : "title"
+					},
+					{
+						title: "content",
+						key : "content"
+					}],
+				diary_show: false
 			}
 		},
 		methods: {
-			diary_list: function(){
-
+			set_diary_list(data){
+				console.log(data)
+				this.diary_list = data
+				console.log(this.diary_list)
 			},
 			get_diary(callback) {
 				var mytoken = this.$store.state.token
@@ -85,26 +70,74 @@
 				}).done(callback)
 			},
 			add_diary(){
-				var _this =  this 
+				//var _this =  this 
 				//var diary_list = this.diary_list
 				//_this.currentdiary = {title:'',content:""},
 				this.create_diary(this.currentdiary, function(){
-					alet("Diary successfully created!")
-					_this.get_diary(function(data) {
-						this.diary_list.push(data)
-						console.log(_this.diary_list)
+					alert("Diary successfully created!")
+						this.get_diary(this.set_diary_list)
 					})
-					_this.diary_add = false
-				})
-			}
+				this.diary_add = false
+			},
+			select_current_diary(currentRow,oldCurrentRow){
+            	console.log(currentRow,oldCurrentRow)
+            	this.currentdiary = currentRow
+            }
 
 		},
-		created : function() {
-			this.get_diary(function(data) {
-				this.diary_list = data
-				this.$nextTick()
+		created: function() {
+			console.log('this is the create' )
+			this.get_diary(this.set_diary_list)
+		},
+		computed: {
+			my_list : function() {
+				console.log('this is the computed' )
 				console.log(this.diary_list)
-			})
+				return this.diary_list
+			}		
 		}
 	}
 </script>
+
+
+<template>
+	<div>
+		<Button type="primary" @click="diary_add = true">Create The Diary</Button>
+		<template>
+			<Table highlight-row ref="currentRowTable" @on-current-change="select_current_diary" 
+			@on-row-dblclick="diary_show = true" :columns="columns1" :data="my_list"></Table>
+		</template>
+	    <Modal
+	        v-model="diary_add"
+	        title="Create the diary"
+	        :loading="loading"
+	        @on-ok="add_diary">
+		    <template>
+				<Form :model="formTop" label-position="left" :label-width="500">
+			        <FormItem label="title">
+			            <Input v-model="currentdiary.title"></Input>
+			        </FormItem>
+			        <FormItem label="content">
+			            <Input v-model="currentdiary.content" type="textarea" :autosize="{minRows: 5,maxRows: 15}" placeholder="Enter something..."></Input>
+			        </FormItem>
+			    </Form>
+			</template>
+	    </Modal>
+	    <Modal
+	        v-model="diary_show"
+	        title="Show diary Detail"
+	        :loading="loading"
+	        @on-ok="diary_show = false">
+		    <template>
+				<div style="background:#eee;padding: 20px">
+				    <Card :bordered="false">
+				        <p slot="title">{{currentdiary.title}}</p>
+				        <p>{{currentdiary.content}}</p>
+				    </Card>
+				    <br/>
+				    <p>日期：{{currentdiary.date}}</p>
+				</div>
+			</template>
+	    </Modal> 
+	</div>
+</template>
---
+++ frontend/src/components/diary/showdiary.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<div v-for="diary in my_list" :key="diary.id">
+		    <div style="background:#eee;padding: 20px" v-on:click="show_detail(diary)" >
+		        <Card :bordered="false" >
+		            <p slot="title">{{diary.title}}</p>
+		            <p>{{diary.content}}</p>
+		            <p>{{diary.date}}</p>
+		        </Card>
+		    </div>
+		</div>
+		<Modal
+	        v-model="diary_show"
+	        title="Create the diary"
+	        :loading="loading"
+	        @on-ok="diary_show = false">
+		    <template>
+				<Form :model="formLeft" label-position="top" :label-width="300">
+			        <FormItem label="title">
+			            <Input v-model="currentdiary.title"></Input>
+			        </FormItem>
+			        <FormItem label="content">
+			            <Input v-model="currentdiary.content" type="textarea" :autosize="{minRows: 5,maxRows: 15}" placeholder="Enter something..."></Input>
+			        </FormItem>
+			    </Form>
+			</template>
+	    </Modal> 
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		name: 'showdiary',
+		props:['my_list'],
+		data(){
+			return {
+				 currentdiary : {title:'',content:""},	
+				 diary_show: false		
+			}
+		},
+		methods: {
+			showdiary(diary){
+				this.currentdiary = diary
+				this.diary_show = true
+			}
+		}
+	}
+</script>
